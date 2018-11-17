f12a76206f31b90903a7a55841fc45ea4987c1bc
dolphin422 <dolphin422@163.com>
2018-11-17 18:07:04 +0800

    commit simple project

---
+++ dolphin-web/src/main/java/com/dolphin422/dolphin/DolphinApplication.java
@@ -0,0 +1,12 @@
+package com.dolphin422.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolphinApplication.class, args);
+	}
+}
---
+++ dolphin-web/src/main/java/com/dolphin422/dolphin/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.dolphin422.dolphin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description:
+ * @Author: jiatai
+ * @CreateDate: 2018.11.17 17:25
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/sayHello")
+    @ResponseBody
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
