78c58dcd5626be3581518f7b2dd469ea4b61af9a
DJT <djt@Damon-MBP.local>
2018-11-28 00:48:29 +0800

    add datasource

--- dolphin-web/src/main/java/com/dolphin422/DolphinApplication.java
+++ dolphin-web/src/main/java/com/dolphin422/DolphinApplication.java
@@ -1,12 +1,11 @@
 package com.dolphin422;
 
-import org.mybatis.spring.annotation.MapperScan;
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan(value = "com.dolphin422")
