a9663496ae787975347295ac1d1f5a5e7b77c014
dolphin422 <dolphin422@163.com>
2018-11-30 18:45:51 +0800

    '增加日志配置'

--- dolphin-system/dolphin-system-controller/src/main/java/com/dolphin422/system/controller/user/SysUserController.java
+++ dolphin-system/dolphin-system-controller/src/main/java/com/dolphin422/system/controller/user/SysUserController.java
@@ -1,18 +1,22 @@
 package com.dolphin422.system.controller.user;
 
-
 import com.dolphin422.system.model.SysUserModel;
 import com.dolphin422.system.service.api.ISysUserService;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/user")
 public class SysUserController {
+    /**
+     * logger
+     */
+    private Logger logger = LoggerFactory.getLogger(SysUserController.class);
 
     @Autowired
     private ISysUserService sysUserService;
@@ -21,8 +25,9 @@ public class SysUserController {
     @RequestMapping("/showDataByPage")
     @ResponseBody
     public List<SysUserModel> showDataByPage() {
-
+        logger.debug("------select start------");
         List<SysUserModel> userModelList = sysUserService.searchListByPage();
+        logger.debug("------select end ------");
         return userModelList;
     }
 }
