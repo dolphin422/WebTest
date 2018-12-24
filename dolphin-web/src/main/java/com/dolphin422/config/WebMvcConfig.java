86b32ad41a9fdcb5c5f10fa86c289978aeb148ff
dolphin422 WIN <dolphin422@163.com>
2018-12-24 18:35:18 +0800

    'Jackson'

--- dolphin-web/src/main/java/com/dolphin422/config/WebMvcConfig.java
+++ dolphin-web/src/main/java/com/dolphin422/config/WebMvcConfig.java
@@ -1,45 +1,52 @@
 package com.dolphin422.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
+import com.dolphin422.common.util.DateUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.DateTypeAdapter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @Description: WebMvc 配置类
  * @author: DamonJT WIN
  * @createDate: 2018.12.13 20:42
  */
-@Configuration
+//@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    /**
-     * 配置消息转换器--这里我用的是alibaba 开源的 fastjson
-     * @param converters
-     */
+    @Bean
+    public Gson gson() {
+        GsonBuilder b = new GsonBuilder();
+        b.setDateFormat(DateUtil.FORMAT_DATE_TIME);
+        b.registerTypeAdapterFactory(DateTypeAdapter.FACTORY);
+        return b.create();
+    }
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        //1.需要定义一个convert转换消息的对象;
+        converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter);
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setWriteAcceptCharset(false);
+        stringConverter.setSupportedMediaTypes(Collections
+            .singletonList(MediaType.TEXT_PLAIN));
+        converters.add(stringConverter);
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(new SourceHttpMessageConverter<>());
         GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
-        //2.添加fastJson的配置信息，比如：是否要格式化返回的json数据;
-
-/*        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
-            SerializerFeature.WriteMapNullValue,
-            SerializerFeature.WriteNullStringAsEmpty,
-            SerializerFeature.DisableCircularReferenceDetect,
-            SerializerFeature.WriteNullListAsEmpty,
-            SerializerFeature.WriteDateUseDateFormat);*/
-        //3处理中文乱码问题
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        //4.在convert中添加配置信息.
-        gsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
-        //gsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-        //5.将convert添加到converters当中.
+        gsonHttpMessageConverter.setGson(gson());
+        gsonHttpMessageConverter.setSupportedMediaTypes(Arrays
+            .asList(MediaType.APPLICATION_JSON));
         converters.add(gsonHttpMessageConverter);
     }
 
